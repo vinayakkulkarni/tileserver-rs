4c3459c1940cd3f3062254f747dfac7dca759290
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2025-12-28 11:53:37 +0530

    chore(homebrew): update formula to v0.2.1
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- homebrew/Formula/tileserver-rs.rb
+++ homebrew/Formula/tileserver-rs.rb
@@ -4,13 +4,13 @@
 class TileserverRs < Formula
   desc "High-performance vector tile server with native MapLibre rendering"
   homepage "https://github.com/vinayakkulkarni/tileserver-rs"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vinayakkulkarni/tileserver-rs/releases/download/tileserver-rs-v#{version}/tileserver-rs-aarch64-apple-darwin.tar.gz"
-      sha256 "TODO"
+      sha256 "e10dacb37203e52fdb461eb3e4ac85a67e2db40bdc71c7899a42821c644f57cb"
     elsif Hardware::CPU.intel?
       url "https://github.com/vinayakkulkarni/tileserver-rs/releases/download/tileserver-rs-v#{version}/tileserver-rs-x86_64-apple-darwin.tar.gz"
       sha256 "TODO"
@@ -23,7 +23,7 @@ class TileserverRs < Formula
       sha256 "TODO"
     elsif Hardware::CPU.intel?
       url "https://github.com/vinayakkulkarni/tileserver-rs/releases/download/tileserver-rs-v#{version}/tileserver-rs-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "TODO"
+      sha256 "d4014342360c2ecee14bea99ee3e6f0ee2d57c57ebc99d7e66a715bebc17997b"
     end
   end
 
